713aba1e35804b8152f4f5ea2711158f43460fbe
ShlomoFriedman <123473216+ShlomoFriedman@users.noreply.github.com>
2024-01-28 16:52:31 +0200

    more css

--- little-lemon/src/Footer.js
+++ little-lemon/src/Footer.js
@@ -4,29 +4,28 @@ import './App.css';
 const Footer = () => {
     return (
         <footer className='app-footer'>
-            <nav>
-                <img src="/Little%20Lemon%20Logo%20Icon.svg" alt="Logo" />
-                <ul>
-                    <li>Doormat Navigation</li>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#reservations">Reservations</a></li>
-                    <li><a href="#order online">Order Online</a></li>
-                    <li><a href="login">Login</a></li>
-                </ul>
-                <ul>
-                    <li>Contact</li>
-                    <li><a href="#address">Address</a></li>
-                    <li><a href="#phone number">Phone Number</a></li>
-                    <li><a href="email">Email</a></li>
-                </ul>
-                <ul>
-                    <il>Social Media</il>
-                    <li><a href="#facebook">Facebook</a></li>
-                    <li><a href="#tiktok">Tiktok</a></li>
-                    <li><a href="Instagram">Instagram</a></li>
-                </ul>
-            </nav>
+            <img src="/Little%20Lemon%20Logo%20Icon.svg" alt="Logo" />
+            <ul className='doormat navigation'>
+                <li className='doormat navigation'>Doormat Navigation</li>
+                <li className='doormat navigation'><a href="#home">Home</a></li>
+                <li className='doormat navigation'><a href="#about">About</a></li>
+                <li className='doormat navigation'><a href="#reservations">Reservations</a></li>
+                <li className='doormat navigation'><a href="#order online">Order Online</a></li>
+                <li className='doormat navigation'><a href="login">Login</a></li>
+            </ul>
+            <ul className='contact'>
+                <li className='contact'>Contact</li>
+                <li className='contact'><a href="#address">Address</a></li>
+                <li className='contact'><a href="#phone number">Phone Number</a></li>
+                <li className='contact'><a href="email">Email</a></li>
+            </ul>
+            <ul>
+                <il className='social media'>Social Media</il>
+                <li className='social media'><a href="#facebook">Facebook</a></li>
+                <li className='social media'><a href="#tiktok">Tiktok</a></li>
+                <li className='social media'><a href="Instagram">Instagram</a></li>
+            </ul>
+            <p>All rights reserved © 2024 Little Lemon Restaurant</p>
         </footer>
     )
 }
--- little-lemon/src/Header.js
+++ little-lemon/src/Header.js
@@ -4,16 +4,14 @@ import './App.css';
 const Header = () => {
     return (
         <header className='app-header'>
-            <nav>
-                <img src="/Little%20Lemon%20Logo.svg" alt="Logo" />
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#reservations">Reservations</a></li>
-                    <li><a href="#order online">Order Online</a></li>
-                    <li><a href="login">Login</a></li>
-                </ul>
-            </nav>
+            <img src="/Little%20Lemon%20Logo.svg" alt="Logo" />
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#reservations">Reservations</a></li>
+                <li><a href="#order online">Order Online</a></li>
+                <li><a href="#login">Login</a></li>
+            </ul>
         </header>
     )
 }
