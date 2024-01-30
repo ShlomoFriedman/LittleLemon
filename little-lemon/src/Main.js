ca67b29c6a417faf7f0236ee779736e6524163b0
ShlomoFriedman <123473216+ShlomoFriedman@users.noreply.github.com>
2024-01-30 14:40:42 +0200

    more css

--- little-lemon/src/Main.js
+++ little-lemon/src/Main.js
@@ -12,26 +12,26 @@ const Main = () => {
             </section>
             <section className='app-main-highlights'>
                 <h1>This week specials</h1>
-                <button>Order Online</button>
-                <article>
+                <article className='meal1'>
                     <img src="/salad.svg" alt="Greek salad"></img>
                     <h3>Greek salad</h3>
                     <p>A harmonious blend of crisp greens, feta, olives, and sun-kissed tomatoes. Simply divine.</p>
                     <h4>Price: $12.90</h4>
                 </article>
-                <article>
+                <article className='meal2'>
                     <img src="/bruchetta.svg" alt="bruchetta"></img>
                     <h3>Bruchetta</h3>
                     <p>Crispy baguette slices topped with ripe tomatoes, garlic, basil, and drizzled olive oil.</p>
                     <h4>Price: $14.99</h4>
                 </article>
-                <article>
+                <article className='meal3'>
                     <img src="/lemon-dessert.svg" alt="lemon dessert"></img>
                     <h3>Lemon Dessert</h3>
                     <p>This comes straight from grandmas recipe book, every last ingredient has been sourced and authenticated.</p>
                     <h4>Price: $12.90</h4>
                 </article>
-            </section><section className='testimonials'>
+                <button>Order Online</button>
+            </section><section className='app-main-testimonials'>
                 <div className="background-image"></div>
                 <h1>What costumers say about us</h1>
                 <article>
